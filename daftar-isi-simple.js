1de8e5494a2c1591fe1cd32bfce0d1b87ea78cb1
fuukoi <33972226+fuukoi@users.noreply.github.com>
2018-03-08 15:16:18 -0300

    Update daftar-isi-simple.js

--- daftar-isi-simple.js
+++ daftar-isi-simple.js
@@ -22,7 +22,6 @@
    var postUrl = new Array();       // array of posturls
    var postDate = new Array();      // array of post publish dates
    var postSum = new Array();       // array of post summaries
-   var postLabels = new Array();    // array of post labels
 
 // global variables
    var sortBy = "datenewest";         // default value for sorting ToC
@@ -104,7 +103,6 @@ function loadtoc(json) {
             postDate.push(postdate);
             postUrl.push(posturl);
             postSum.push(postcontent);
-            postLabels.push(pll);
          }
       }
       if(totalEntires==totalPosts) {tocLoaded=true;showToc();}
@@ -161,9 +159,6 @@ function sortPosts(sortBy) {
       var temp = postSum[x];
       postSum[x] = postSum[y];
       postSum[y] = temp;
-      var temp = postLabels[x];
-      postLabels[x] = postLabels[y];
-      postLabels[y] = temp;
    } // end swapPosts
 
    for (var i=0; i < postTitle.length-1; i++) {
