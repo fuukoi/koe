98e80d97d9a6fd3cb52d11922b97eb733635fa5c
fuukoi <33972226+fuukoi@users.noreply.github.com>
2019-12-28 13:29:58 -0300

    demo 3

--- src/script.js
+++ src/script.js
@@ -1,44 +1,4 @@
-/* C A R D */
-const card = document.querySelector('.card');
-const text = document.querySelector('.card-text');
-let p;
 
-function mouseMove(e){
-    let mX = e.clientX,
-        mY = e.clientY,
-        lX = (mX - p.x),
-        tY = (mY - p.y),
-        c  = {
-            x: lX - p.width / 2,
-            y: tY - p.height / 2
-        },
-        d  = Math.sqrt(c.x**2 + c.y**2);
-
-    card.style.transform = 'scale3d(1.07, 1.07, 1.07) rotate3d('+(c.y / 100)+', '+(-c.x / 100)+', 0, '+(Math.log(d) * 2)+'deg)';
-    
-    text.style.transform = 'scale3d(1.07, 1.07, 1.07) rotate3d('+(c.y / 100)+', '+(-c.x / 100)+', 0, '+(Math.log(d) * 2)+'deg)';
-
-    card.querySelector('.glow').style.backgroundImage = `
-    radial-gradient(
-      circle at
-      ${c.x * 2 + p.width/2}px
-      ${c.y * 2 + p.height/2}px,
-      #ffffff55,
-      #0000000f
-    )
-  `;
-}
-
-card.onmouseover = function(){
-    p = card.getBoundingClientRect();
-    document.addEventListener('mousemove', mouseMove);
-}
-card.addEventListener('mouseleave', () => {
-    document.removeEventListener('mousemove', mouseMove);
-    card.style.transform = '';
-    card.style.background = '';
-  });
-/* C A R D   E N D */ 
 
 
 
